169488380af64448da1e89222f9de0c90831837f
Muhammad Hamza <hafizhamza2247@gmail.com>
2023-08-25 22:20:13 +0500

    form

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -13,13 +13,22 @@ export default function TextForm(props) {
     let newText = '';
     setText(newText);
   };
+  const handleCopy = ()=>{
+    let text = document.getElementById('myBox')
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  };
+  const handelExtraSpace = () =>{
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(' '))
+  }
   const handelOnChange = (e) => {
     setText(e.target.value);
   };
-  const extractEmails = () => {
-    let newText = text.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/gi);
-    setText(newText);
-    };
+  // const extractEmails = () => {
+  //   let newText = text.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/gi);
+  //   setText(newText);
+  //   };
     
   const [text, setText] = useState("");
   return (
@@ -33,6 +42,8 @@ export default function TextForm(props) {
       <button onClick={handelLowClick} className="btn btn-primary mx-1">Convert to upper case</button>
       {/* <button onClick={extractEmails} className="btn btn-primary mx-1">Extract email</button> */}
       <button onClick={handelClearClick} className="btn btn-primary mx-1">Clear</button>
+      <button onClick={handleCopy} className="btn btn-primary mx-1">Copy Text</button>
+      <button onClick={handelExtraSpace} className="btn btn-primary mx-1">Extra Space</button>
     </div>
     <div className="container my-3">
     <h1>Your text sumery</h1>
